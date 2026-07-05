6dbed17d36fe03908cff48f1970351d99c8d9cb9
aashleshagit <aashleshasawane1@gmail.com>
2026-07-05 18:53:14 +0530

    Create Stats.js

---
+++ src/Stats.js
@@ -0,0 +1,31 @@
+function Stats() {
+    return (
+
+        <section className="stats">
+
+            <div className="stat-card">
+                <h1>6+</h1>
+                <p>Projects</p>
+            </div>
+
+            <div className="stat-card">
+                <h1>5+</h1>
+                <p>Certifications</p>
+            </div>
+
+            <div className="stat-card">
+                <h1>2027</h1>
+                <p>Graduation</p>
+            </div>
+
+            <div className="stat-card">
+                <h1>100%</h1>
+                <p>Passion</p>
+            </div>
+
+        </section>
+
+    );
+}
+
+export default Stats;
